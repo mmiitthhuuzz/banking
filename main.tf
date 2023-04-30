8ebd32914fbc513070c5611480a2fc836959866a
root <root@ip-172-31-5-68.ap-south-1.compute.internal>
2023-04-30 09:01:02 +0000

    modify

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "prod-server" {
+resource "aws_instance" "test-server" {
   ami           = "ami-0c768662cc797cd75" 
   instance_type = "t2.micro" 
   key_name = "Keypair"
@@ -7,16 +7,16 @@ resource "aws_instance" "prod-server" {
     type     = "ssh"
     user     = "ubuntu"
     private_key = file("./Keypair.pem")
-    host     = aws_instance.prod-server.public.ip
+    host     = aws_instance.test-server.public.ip
   }
   provisioner "local-exec" {
     command = "sleep 60 && echo 'Instance ready'"
   }
   tags = {
-    Name = "prod-server"
+    Name = "test-server"
   }
   provisioner "local-exec" {
-        command = " echo ${aws_instance.prod-server.public_ip} > inventory "
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
    provisioner "local-exec" {
   command = "ansible-playbook /var/lib/jenkins/workspace/banking/test-server/finance-playbook.yml "
